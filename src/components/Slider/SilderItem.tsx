a7bb0e1bd52b9756e7bc8d179a89f6d82e473ea5
Rafael Morais <rafahgmorais@gmail.com>
2020-10-22 00:22:29 -0300

    started animations from screatch

--- src/components/Slider/SilderItem.tsx
+++ src/components/Slider/SilderItem.tsx
@@ -5,16 +5,21 @@ import "../style/Slider/SliderItem.scss";
 type SliderItemProps = {
   movie: Movie;
   width: number;
+  text: string;
 };
 
 const SliderItem: React.FC<SliderItemProps> = ({
   movie,
   width,
+  text,
 }: SliderItemProps) => {
   return (
     <div className="slider-item" style={{ width: `${width}%` }}>
+      <div className="slider-item-overlay">
+        <span>{text}</span>
+      </div>
       <img
-        className="slider-image"
+        className="slider-item-image"
         src={`http://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
         alt={movie.title}
       />
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SliderItem from "./SilderItem";
 import SliderControl from "./SliderControl";
+import useWindowDimensions from "../../util/windowDimensions";
 
 import "../style/Slider/Slider.scss";
 
@@ -8,44 +9,65 @@ type SliderProps = {
   movies: Movie[];
 };
 
+type SliderState = {
+  hasMoved: boolean;
+  moveDirection: string;
+  isMoving: boolean;
+  movePercentage: number;
+  leftIndex: number;
+  showSize: number;
+};
+
 const Slider: React.FC<SliderProps> = ({ movies }: SliderProps) => {
-  const [hasMoved, setHasMoved] = useState(false);
-  const [moveDirection, setMoveDirection] = useState("");
-  const [isMoving, setIsMoving] = useState(false);
-  const [movePercentage, setMovePercentage] = useState(0);
-  const [lowestVisibleIndex, setLowestVisibleIndex] = useState(0);
-  const [itemsRow, setItemsRow] = useState(6);
+  const [state, setState] = useState<SliderState>({
+    hasMoved: false,
+    moveDirection: "",
+    isMoving: false,
+    movePercentage: 0,
+    leftIndex: 0,
+    showSize: 4,
+  });
+
+  const windowDimensions = useWindowDimensions();
+
+  const updateState = (newState: any) => {
+    setState({ ...state, ...newState });
+  };
+
+  if (!movies.length) return null;
+
+  let totalItems = movies.length;
 
   const renderContent = () => {
-    console.log(lowestVisibleIndex);
-    let totalItems = movies.length;
+    console.log("Render");
 
-    const left = [];
-    const center = [];
-    const right = [];
+    const left: number[] = [];
+    const center: number[] = [];
+    const right: number[] = [];
 
-    for (let i = 0; i < itemsRow; i++) {
-      // left
+    let hasMoved = state.hasMoved;
+    let leftIndex = state.leftIndex;
+    let showSize = state.showSize;
+
+    for (let i = 0; i < showSize; i++) {
+      //left
       if (hasMoved) {
-        if (lowestVisibleIndex + i - itemsRow < 0) {
-          left.push(totalItems - itemsRow + lowestVisibleIndex + i);
-        } else {
-          left.push(lowestVisibleIndex + i - itemsRow);
+        if (leftIndex === 0) {
+          left.push(showSize * 2 + i);
         }
+        left.push(leftIndex - showSize + i);
       }
 
-      // center
-      if (i + lowestVisibleIndex >= totalItems) {
-        center.push(i + lowestVisibleIndex - totalItems);
-      } else {
-        center.push(i + lowestVisibleIndex);
-      }
+      // TODO: Resolver probelema de quando o número de itens não for multiplo do número de itens na tela
+      //center
+      center.push(leftIndex + i);
 
-      // right
-      if (i + lowestVisibleIndex + itemsRow >= totalItems) {
-        right.push(i + lowestVisibleIndex + itemsRow - totalItems);
+      //right
+      console.log(leftIndex + showSize);
+      if (leftIndex + showSize >= totalItems) {
+        right.push(i);
       } else {
-        right.push(i + lowestVisibleIndex + itemsRow);
+        right.push(leftIndex + showSize + i);
       }
     }
 
@@ -53,111 +75,49 @@ const Slider: React.FC<SliderProps> = ({ movies }: SliderProps) => {
     console.log("Center", center);
     console.log("Right", right);
 
-    const combinedIndex = [...left, ...center, ...right];
-    console.log("Combined Index", combinedIndex);
-
-    if (hasMoved) {
-      const trailingIndex =
-        combinedIndex[combinedIndex.length - 1] === totalItems - 1
-          ? 0
-          : combinedIndex[combinedIndex.length - 1] + 1;
-      combinedIndex.push(trailingIndex);
-    }
+    const combinedIndex: number[] = [...left, ...center, ...right];
 
     const sliderContent = [];
 
     for (let index of combinedIndex) {
       sliderContent.push(
-        <SliderItem key={index} movie={movies[index]} width={100 / itemsRow} />
+        <SliderItem
+          key={index}
+          movie={movies[index]}
+          width={100 / state.showSize}
+          text={index.toString()}
+        />
       );
-    }
-
-    if (!hasMoved) {
-      for (let i = 0; i < itemsRow; i++) {
-        sliderContent.unshift(
-          <div
-            key={i + 100}
-            className="slider-item"
-            style={{ width: `${100 / itemsRow}%` }}
-          ></div>
-        );
-      }
     }
 
     return sliderContent;
   };
 
   const handleNext = () => {
-    const totalItems = movies.length;
-
-    let newIndex: number;
-    if (lowestVisibleIndex === totalItems - itemsRow) {
-      newIndex = 0;
-    } else if (lowestVisibleIndex + itemsRow > totalItems - itemsRow) {
-      newIndex = totalItems - itemsRow;
-    } else {
-      newIndex = lowestVisibleIndex + itemsRow;
-    }
-
-    let newMovePercentage;
-    if (newIndex !== 0) {
-      newMovePercentage = ((newIndex - lowestVisibleIndex) / itemsRow) * 100;
-    } else {
-      newMovePercentage = 100;
-    }
-
-    setIsMoving(true);
-    setMoveDirection("right");
-    setMovePercentage(newMovePercentage);
-
-    setTimeout(() => {
-      setLowestVisibleIndex(newIndex);
-      setIsMoving(false);
-      setMoveDirection("");
-      setMovePercentage(0);
-    }, 750);
-
-    if (!hasMoved) setHasMoved(true);
+    updateState({
+      leftIndex: state.leftIndex + state.showSize,
+      hasMoved: true,
+    });
   };
 
-  const handlePrev = () => {
-    const totalItems = movies.length;
-    let newIndex: number;
-    if (lowestVisibleIndex < itemsRow && lowestVisibleIndex !== 0) {
-      newIndex = 0;
-    } else if (lowestVisibleIndex - itemsRow < 0) {
-      newIndex = totalItems - itemsRow;
-    } else {
-      newIndex = lowestVisibleIndex - itemsRow;
-    }
+  const handlePrev = () => {};
 
-    let newMovePercentage;
-    if (lowestVisibleIndex === 0) {
-      newMovePercentage = 0;
-    } else if (lowestVisibleIndex - newIndex < itemsRow) {
-      newMovePercentage =
-        ((itemsRow - (lowestVisibleIndex - newIndex)) / itemsRow) * 100;
-    } else {
-      newMovePercentage = 0;
-    }
+  let style: React.CSSProperties = {};
 
-    setIsMoving(true);
-    setMoveDirection("left");
-    setMovePercentage(newMovePercentage);
+  if (state.hasMoved) {
+    style = {
+      transform: `translateX(-${window.innerWidth}px)`,
+    };
+  }
 
-    setTimeout(() => {
-      setLowestVisibleIndex(newIndex);
-      setIsMoving(false);
-      setMoveDirection("");
-      setMovePercentage(0);
-    }, 750);
-  };
   return (
     <div className="slider">
-      {hasMoved && <SliderControl arrowDirection="left" onClick={handlePrev} />}
-      {movies.length > 0 && (
-        <div className="slider-content">{renderContent()}</div>
+      {state.hasMoved && (
+        <SliderControl arrowDirection="left" onClick={handlePrev} />
       )}
+      <div className="slider-content" style={style}>
+        {renderContent()}
+      </div>
       <SliderControl arrowDirection="right" onClick={handleNext} />
     </div>
   );
