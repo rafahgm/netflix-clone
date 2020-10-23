9ad4604bd2b34e9a43f47b57731b0dbf0fc2b299
Rafael Morais <rafahgmorais@gmail.com>
2020-10-23 15:08:47 -0300

    controls below slider item done

--- src/components/Slider/SliderItemControls.tsx
+++ src/components/Slider/SliderItemControls.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Icon } from "@iconify/react";
 import playIcon from "@iconify/icons-mdi/play";
@@ -6,8 +6,15 @@ import plusIcon from "@iconify/icons-mdi/plus";
 import thumbUpOutline from "@iconify/icons-mdi/thumb-up-outline";
 import thumbDownOutline from "@iconify/icons-mdi/thumb-down-outline";
 import chevronDown from "@iconify/icons-mdi/chevron-down";
+import axios from "axios";
 
+import rating18 from "../../assets/18.png";
+import rating16 from "../../assets/16.png";
+import rating14 from "../../assets/14.png";
+import rating12 from "../../assets/12.png";
 import "../style/Slider/SliderItemControls.scss";
+
+const ratings = [rating12, rating14, rating16, rating18];
 
 type SliderItemControlsProps = {
   movie: Movie;
@@ -33,8 +40,18 @@ export default function SliderItemControls({ movie }: SliderItemControlsProps) {
           <Icon icon={chevronDown} />
         </div>
       </div>
-      <div className="sic-info">{`${movie.vote_average * 10}% Match`}</div>
-      <div className="sic-genres"></div>
+      <div className="sic-info">
+        <span>{`${movie.vote_average * 10}% Match`}</span>
+        <img src={ratings[Math.floor(Math.random() * 4)]} alt="" />
+        <span className="duration">
+          {Math.floor(Math.random() * 3)}h&nbsp;
+          {Math.floor(Math.random() * 60)}
+          min
+        </span>
+      </div>
+      <div className="sic-genres">
+        {/* TODO: Implement genres as a global context so just one call to the api is made */}
+      </div>
     </div>
   );
 }
